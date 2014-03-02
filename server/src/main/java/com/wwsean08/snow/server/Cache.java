3c9ab7f7598ffe67eae9ebbd1ecff581bc8e35ae
Sean Smith <admin@wwsean08.com>
2014-03-01 22:10:44 -0500

    initial stubbed out cache implementation

---
+++ server/src/main/java/com/wwsean08/snow/server/Cache.java
@@ -0,0 +1,93 @@
+package com.wwsean08.snow.server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.wwsean08.snow.common.ReportPOJO;
+
+/**
+ * The cache is actually an in memory database being run by an h2 database. the
+ * basic schema of the table which will store the cached items is <primary key>,
+ * <update time>, <resort id>, <json data>. If there is a row in the database
+ * for the item and it has not expired that will be returned, otherwise a rest
+ * request will be performed to get the item and it will be persisted in the
+ * database.
+ * 
+ * @author wwsea_000
+ * 
+ */
+public class Cache
+{
+    private final String DB_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+    private Connection connection;
+    
+    public Cache() throws SQLException, ClassNotFoundException
+    {
+        Class.forName("org.h2.Driver");
+        // TODO: insert actual credentials before deploying
+        Connection conn = DriverManager.
+                getConnection("jdbc:h2:~/test", "sa", "");
+        connection = conn;
+        // add application code here
+    }
+    
+    /**
+     * Checks the cache for a report within a certain timeframe (TBD) for the
+     * given id, if none returns null.
+     * 
+     * @param id
+     * @return
+     */
+    public ReportPOJO getReportFromCache(int id)
+    {
+        return null;
+    }
+    
+    /**
+     * Inserts a new report into the database for future use by the cace
+     * 
+     * @param report
+     */
+    public void insertReportIntoCache(ReportPOJO report)
+    {
+        
+    }
+    
+    /**
+     * Clears out old and unused data from time to time. This should prevent
+     * lots of memory being used by the in memory database
+     */
+    public void cleanup()
+    {
+        
+    }
+    
+    /**
+     * @throws SQLException
+     */
+    public void closeConnection() throws SQLException
+    {
+        connection.close();
+    }
+    
+    @Override
+    public void finalize()
+    {
+        try
+        {
+            if (!connection.isClosed())
+            {
+                connection.close();
+            }
+            else
+            {
+                connection = null;
+            }
+        }
+        catch (SQLException e)
+        {
+            
+        }
+    }
+}
